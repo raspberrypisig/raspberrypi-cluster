b52b68d7396bf04cea2497ea345a33b668985566
raspberrypisig <raspberrypisig@gmail.com>
2019-10-22 13:45:53 +1100

    Update install-k3s-slave.sh

--- install-k3s-slave.sh
+++ install-k3s-slave.sh
@@ -22,7 +22,7 @@ EOF
 K3S_SERVER_IP=$(getent ahostsv4 $MASTER_NODE|head -1|awk '{print $1}')
 curl -sfL https://get.k3s.io | K3S_URL=https://$K3S_SERVER_IP:6443 K3S_TOKEN=$TOKEN sh -
 
-sed -ri 's/.*K3S_TOKEN=[^ ]*/K3S_TOKEN=$K3S_SERVER_IP/' /etc/systemd/system/k3s-agent.service.env
+sed -ri "s/K3S_TOKEN=[0-9.]+/K3S_TOKEN=$K3S_SERVER_IP/" /etc/systemd/system/k3s-agent.service.env
 
 
 
