2e36296e17cdfec66db740cb244b4941ed3d6eac
raspberrypisig <raspberrypisig@gmail.com>
2019-10-27 13:10:34 +1100

    Update setup-raspbian-lite.sh

--- setup-raspbian-lite.sh
+++ setup-raspbian-lite.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-raspi-config nonint do_hostname k3smaster
+SSID=mycrib
+PASSPHRASE=peachspeak38
+
+HOSTNAME=k3smaster
+KEYBOARD=us
+LOCALE="en_AU.UTF-8"
+TIMEZONE="Australia/Melbourne"
+
+raspi-config nonint do_hostname $HOSTNAME
 raspi-config nonint do_ssh 0
-raspi-config nonint do_configure_keyboard us
-raspi-config nonint do_change_locale en_AU.UTF-8
-raspi-config nonint do_wifi_ssid_passphrase SSID passphrase
-raspi-config nonint do_change_timezone Australia/Melbourne
+raspi-config nonint do_configure_keyboard $KEYBOARD
+raspi-config nonint do_change_locale $LOCALE
+raspi-config nonint do_wifi_ssid_passphrase $SSID $PASSPHRASE
+raspi-config nonint do_change_timezone $TIMEZONE
 raspi-config nonint do_boot_behaviour B2
 
 
