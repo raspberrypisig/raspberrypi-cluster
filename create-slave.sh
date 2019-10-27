bd202142e662bc6608c63859f629129cbd6fc9a8
raspberrypisig <raspberrypisig@gmail.com>
2019-10-27 16:11:46 +1100

    Update create-slave.sh

--- create-slave.sh
+++ create-slave.sh
@@ -1,2 +1,2 @@
 # Run as root
-curl -sSL https://raw.githubusercontent.com/raspberrypisig/raspberrypi-cluster/master/get.sh | NODE_ROLE=slave bash -
+curl -sSL https://raw.githubusercontent.com/raspberrypisig/raspberrypi-cluster/master/get.sh | NODE_ROLE=slave bash - | tee slave.log
