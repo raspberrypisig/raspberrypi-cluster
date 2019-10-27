ee1dc2a0feff40a18595dca4d1bd041f2f0ff49a
raspberrypisig <raspberrypisig@gmail.com>
2019-10-27 14:59:18 +1100

    Update get.sh

--- get.sh
+++ get.sh
@@ -8,5 +8,5 @@ git clone https://github.com/raspberrypisig/raspberrypi-cluster
 cd raspberrypi-cluster
 chmod 744 ./setup-raspbian-lite.sh
 ./setup-raspbian-lite.sh
-$NODE_ROLE = "MASTER" && ./install-k3s-master.sh  
-$NODE_ROLE = "SLAVE" && ./install-k3s-slave.sh
+test $NODE_ROLE = "master" && ./install-k3s-master.sh  
+test $NODE_ROLE = "slave" && ./install-k3s-slave.sh
