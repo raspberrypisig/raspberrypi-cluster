5a8a8d9db1196d612b5f56e25109a7035f5e25de
raspberrypisig <raspberrypisig@gmail.com>
2019-10-27 12:56:20 +1100

    Create setup-raspbian-lite.sh

---
+++ setup-raspbian-lite.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+raspi-config nonint do_hostname k3smaster
+raspi-config nonint do_ssh 0
+raspi-config nonint do_configure_keyboard us
+raspi-config nonint do_change_locale en_AU.UTF-8
+raspi-config nonint do_wifi_ssid_passphrase SSID passphrase
+raspi-config nonint do_change_timezone Australia/Melbourne
