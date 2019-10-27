f43200e9a281bfe0b1446808bf6fe8c48e91cbe6
raspberrypisig <raspberrypisig@gmail.com>
2019-10-27 13:02:05 +1100

    Update setup-raspbian-lite.sh

--- setup-raspbian-lite.sh
+++ setup-raspbian-lite.sh
@@ -6,3 +6,6 @@ raspi-config nonint do_configure_keyboard us
 raspi-config nonint do_change_locale en_AU.UTF-8
 raspi-config nonint do_wifi_ssid_passphrase SSID passphrase
 raspi-config nonint do_change_timezone Australia/Melbourne
+raspi-config nonint do_boot_behaviour B2
+
+
