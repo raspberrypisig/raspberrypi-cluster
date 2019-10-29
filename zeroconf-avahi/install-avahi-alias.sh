24722d3b1bef829a0299e684362a0e926cee9b6f
raspberrypisig <raspberrypisig@gmail.com>
2019-10-29 19:44:57 +1100

    Create install-avahi-alias.sh

---
+++ zeroconf-avahi/install-avahi-alias.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+apt install -y python-avahi
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+install -m 755 avahi-alias.py /usr/local/bin
+cp avahi-alias.service /etc/systemd/system/
+sed -ri 's/HOSTNAMES/nginx-k3smaster.local/' /etc/systemd/system/avahi-alias.service
+systemctl daemon-reload
+systemctl enable avahi-alias
+systemctl start avahi-alias
+
