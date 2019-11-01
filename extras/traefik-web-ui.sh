c2023e355e89d2d4cecf26865b967415119858d2
raspberrypisig <raspberrypisig@gmail.com>
2019-11-01 18:07:14 +1100

    Update traefik-web-ui.sh

--- extras/traefik-web-ui.sh
+++ extras/traefik-web-ui.sh
@@ -15,3 +15,21 @@ kubectl -n kube-system patch configmap traefik --patch "$(cat traefik-patch.yaml
 #kubectl get configmap traefik -o yaml -n kube-system|sed -e '/defaultEntryPoints/a\    [api]\n\ \     entryPoint = traefik\n\      dashboard = true' -e '/\[entryPoints\]/a\      \[entryPoints.traefik\]\n\      address = :"8080"'|kubectl apply -f - 
 kubectl -n kube-system delete pod $(kubectl -n kube-system get pods | awk '/^traefik/ {print $1}')
 #kubectl rollout restart deployment/traefik -n kube-system
+sleep 60
+
+cat<<EOF | kubectl create -f -
+apiVersion: extensions/v1beta1
+kind: Ingress
+metadata:
+  name: traefik-web-ingress
+  namespace: kube-system
+spec:
+  rules:
+  - host: traefik-k3smaster.local
+    http:
+      paths:
+      - path: /
+        backend:
+          serviceName: traefik
+          servicePort: 8080
+EOF
