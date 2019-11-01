911aaddc7d25018e08aac9e5ebac88fe78d70c13
raspberrypisig <raspberrypisig@gmail.com>
2019-11-01 16:50:10 +1100

    Update traefik-web-ui.sh

--- extras/traefik-web-ui.sh
+++ extras/traefik-web-ui.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -xe
+set -x
 
 kubectl -n kube-system patch configmap traefik --patch "$(cat traefik-patch.yaml)"
 #kubectl get configmap traefik -o yaml -n kube-system|sed -e '/defaultEntryPoints/a\    [api]\n\ \     entryPoint = traefik\n\      dashboard = true' -e '/\[entryPoints\]/a\      \[entryPoints.traefik\]\n\      address = :"8080"'|kubectl apply -f - 
