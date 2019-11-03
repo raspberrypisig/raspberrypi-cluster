1f6e9b6dccfb42ec2408a7cd21cd39c36a197c19
raspberrypisig <raspberrypisig@gmail.com>
2019-11-04 10:07:47 +1100

    Update traefik-web-ui.sh

--- extras/traefik-web-ui.sh
+++ extras/traefik-web-ui.sh
@@ -21,10 +21,8 @@ do
   sleep 30
 done
 
-kubectl -n kube-system patch configmap traefik --patch "$(cat traefik-patch.yaml)"
-#kubectl get configmap traefik -o yaml -n kube-system|sed -e '/defaultEntryPoints/a\    [api]\n\ \     entryPoint = traefik\n\      dashboard = true' -e '/\[entryPoints\]/a\      \[entryPoints.traefik\]\n\      address = :"8080"'|kubectl apply -f - 
+kubectl -n kube-system patch configmap traefik --patch "$(cat traefik-patch.yaml)" 
 kubectl -n kube-system delete pod $(kubectl -n kube-system get pods | awk '/^traefik/ {print $1}')
-#kubectl rollout restart deployment/traefik -n kube-system
 sleep 60
 
 cat<<EOF | kubectl apply -f -
