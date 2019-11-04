051d2765db72f8ff7a0a3e6bc396af8818f8be67
raspberrypisig <raspberrypisig@gmail.com>
2019-11-05 10:58:12 +1100

    Update install-avahi-subdomains.sh

--- zeroconf-avahi/install-avahi-subdomains.sh
+++ zeroconf-avahi/install-avahi-subdomains.sh
@@ -11,7 +11,7 @@ systemctl restart avahi-daemon
 
 cat<<EOF > /usr/local/bin/avahi-subdomain
 #!/usr/bin/env bash
-IP=$(getent ahostsv4 k3smaster.local | head -1 | awk '{print $1}')
+IP=\$(getent ahostsv4 k3smaster.local | head -1 | awk '{print $1}')
 SUBDOMAINS=$(cat SUBDOMAINS_K3SMASTER)
 for subdomain in $SUBDOMAINS
 do
