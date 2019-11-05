b5eaff0afaab2b147847292d3cc6ea87177e5625
raspberrypisig <raspberrypisig@gmail.com>
2019-11-05 16:06:33 +1100

    Update install-extras.sh

--- extras/install-extras.sh
+++ extras/install-extras.sh
@@ -3,10 +3,4 @@ set -xe
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
-for script in `ls *.sh`
-do
-  if [ ! $script = $0 ];
-  then
-    bash "$script"
-  fi
-done
+bash traefik-web-ui.sh
