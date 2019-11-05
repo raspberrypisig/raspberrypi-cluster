155bcd5d5dc905b5eda78814038259da61715da8
raspberrypisig <raspberrypisig@gmail.com>
2019-11-05 20:44:31 +1100

    Update install-k3s-slave.sh

--- install-k3s-slave.sh
+++ install-k3s-slave.sh
@@ -21,33 +21,3 @@ EOF
 
 K3S_SERVER_IP=$(getent ahostsv4 $MASTER_NODE|head -1|awk '{print $1}')
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_VERSION="v0.9.1" K3S_URL=https://$K3S_SERVER_IP:6443 K3S_TOKEN=$TOKEN sh -
-
-cat<<EOF > /usr/local/bin/hack-k3s
-#!/usr/bin/env bash
-sleep 30
-MASTER_NODE=$MASTER_NODE
-K3S_SERVER_IP=\$(getent ahostsv4 $MASTER_NODE|head -1|awk '{print \$1}')
-sed -ri "s/K3S_URL=.*/K3S_URL=https:\/\/$K3S_SERVER_IP:6443/" /etc/systemd/system/k3s-agent.service.env
-EOF
-chmod 777 /etc/init.d/hack-k3s
-
-cat<<EOF > /etc/systemd/system/hack-k3s.service
-[Unit]
-[Unit]
-Description=Hack needed because k3s https certs issue 
-Before=k3s-agent.service
-After=network-online.target
-
-[Service]
-Type=exec
-ExecStart=/usr/local/bin/hack-k3s
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-systemctl daemon-reload
-systemctl enable hack-k3s
-reboot
-
-
