c495f095ff46b2b244256e2af84bc6c811bb687a
raspberrypisig <raspberrypisig@gmail.com>
2019-11-07 09:37:59 +1100

    Update llmnr.sh

--- zeroconf-avahi/llmnr.sh
+++ zeroconf-avahi/llmnr.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 cp llmnr.py /usr/local/bin
 cp llmnr.service /etc/system/system
+mkdir -p /etc/llmnr
+cp SUBDOMAINS_K3SMASTER /etc/llmnr
 systemctl daemon-reload
 systemctl enable llmnr
 systemctl start llmnr
