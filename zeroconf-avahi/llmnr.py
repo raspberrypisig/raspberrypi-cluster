a73e6ed0ad62f1bd446aeae6b4b50343ca6056e4
raspberrypisig <raspberrypisig@gmail.com>
2019-11-08 17:04:25 +1100

    Update llmnr.py

--- zeroconf-avahi/llmnr.py
+++ zeroconf-avahi/llmnr.py
@@ -37,15 +37,15 @@ while 1:
         if str(hn).find('k3smaster') == -1:
             continue
         #print(hn)
-        subdomains = Path('/etc/llmnr/SUBDOMAINS_K3SMASTER').read_text().split('\n')
-        if '' in subdomains:
-            subdomains.remove('')
-        subdomains = subdomains + ["k3smaster"]
-        boo=[str(hn).find(i)!=-1 for i in subdomains]
-        if True in boo or str(hn) == "k3smaster":
-            pass
-        else:
-            continue
+        #subdomains = Path('/etc/llmnr/SUBDOMAINS_K3SMASTER').read_text().split('\n')
+        #if '' in subdomains:
+        #    subdomains.remove('')
+        #subdomains = subdomains + ["k3smaster"]
+        #boo=[str(hn).find(i)!=-1 for i in subdomains]
+        #if True in boo or str(hn) == "k3smaster":
+        #    pass
+        #else:
+        #    continue
     except socket.error as e:
         pass
     else:
