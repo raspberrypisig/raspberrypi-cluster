5216d3aa878c1fff7edade896a712a38d9301572
raspberrypisig <raspberrypisig@gmail.com>
2019-11-13 19:16:37 +1100

    Update install-k3s-master.sh

--- install-k3s-master.sh
+++ install-k3s-master.sh
@@ -14,7 +14,7 @@ su pi bash -c "touch /home/pi/.hushlogin"
 rm /etc/profile.d/sshpwd.sh
 
 # Install k3s as master node
-curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_VERSION="v0.9.1" sh -s -
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_VERSION="v0.11.0-alpha2" sh -s -
 
 sleep 400
 
