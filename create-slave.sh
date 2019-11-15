1aaa9978d765143f4405f97734e7d63aae33f0d5
raspberrypisig <raspberrypisig@gmail.com>
2019-11-16 09:55:09 +1100

    Update create-slave.sh

--- create-slave.sh
+++ create-slave.sh
@@ -1,3 +1,3 @@
 # Run as root
 read -p "Enter new hostname for slave node: " hn
-curl -sSL https://raw.githubusercontent.com/raspberrypisig/raspberrypi-cluster/experimental/get.sh | NODE_ROLE=slave NEWHOSTNAME=$hn bash - | tee slave.log
+curl -sSL https://raw.githubusercontent.com/raspberrypisig/raspberrypi-cluster/master/get.sh | NODE_ROLE=slave NEWHOSTNAME=$hn bash - | tee slave.log
