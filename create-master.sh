715d9cf705821daca3095e325a9d3d34e39d6a54
raspberrypisig <raspberrypisig@gmail.com>
2019-11-16 10:22:23 +1100

    Update create-master.sh

--- create-master.sh
+++ create-master.sh
@@ -1,2 +1,2 @@
 # Run as root
-curl -sSL https://raw.githubusercontent.com/raspberrypisig/raspberrypi-cluster/experimental/get.sh | NODE_ROLE=master bash - | tee master.log
+curl -sSL https://raw.githubusercontent.com/raspberrypisig/raspberrypi-cluster/master/get.sh | NODE_ROLE=master bash - | tee master.log
